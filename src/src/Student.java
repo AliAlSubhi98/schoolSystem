21305f23b6e7738663a990cb256cd794a8024917
Ali AlSubhi <alialsubhi1998@gmail.com>
2023-01-30 23:03:18 -0800

    TranscriptDownload

---
+++ src/src/Student.java
@@ -0,0 +1,11 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Student {
+	ArrayList<Subject> studentSubjectList = new ArrayList<Subject>();
+	HashSet<String> emailAddresses = new HashSet<String>();
+    String name;
+    String mail;
+}
